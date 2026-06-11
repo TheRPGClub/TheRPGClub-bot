a27463bc44665b5400a57d1a58aaa474030dac07
merph518 <merph2007@gmail.com>
2026-06-11 08:28:50 -0400

    updates roles

--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,9 +1,15 @@
 export const REGULARS_ROLE_ID = "267883288817958912";
+export const REGULARS_ROLE_NAME = "regulars";
+
 export const NEWCOMERS_ROLE_ID = "927710610488176670";
-export const ADMIN_ROLE_ID =
-  process.env.ADMIN_ROLE_ID?.replace(/[<@&>]/g, "").trim() ?? null;
-export const MODERATOR_ROLE_ID =
-  process.env.MODERATOR_ROLE_ID?.replace(/[<@&>]/g, "").trim() ?? null;
+export const NEWCOMERS_ROLE_NAME = "newcomers";
+
+export const ADMIN_ROLE_ID = "461891227613265940";
+export const ADMIN_ROLE_NAME = "admins";
+
+export const MODERATOR_ROLE_ID = "267881447258325002";
+export const MODERATOR_ROLE_NAME = "moderators";
+
 export const MEMBER_ROLE_ID = "747520789003239530";
 export const MEMBER_ROLE_NAME = "members";
-export const NEWCOMERS_ROLE_NAME = "newcomers";
+
