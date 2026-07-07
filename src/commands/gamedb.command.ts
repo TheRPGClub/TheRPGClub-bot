a1832d5cb7364580447533e5e58b6a429948c441
merph518 <merph2007@gmail.com>
2026-07-07 14:02:19 -0400

    removed gamedb csv import, that's all handled by the API now

--- src/commands/gamedb.command.ts
+++ src/commands/gamedb.command.ts
@@ -1,5 +1,4 @@
 export { GameDbAddCommand } from "./gamedb/gamedb-add.command.js";
-export { GameDbCsvImportCommand } from "./gamedb/gamedb-csv-import.command.js";
 export { GameDbViewCommand } from "./gamedb/gamedb-view.command.js";
 export { GameDbSearchCommand } from "./gamedb/gamedb-search.command.js";
 export { GameDbCompletionCommand } from "./gamedb/gamedb-completion.command.js";
--- src/db/sql/index.ts
+++ src/db/sql/index.ts
@@ -3,8 +3,6 @@ export type { ISqlEntry } from "./types.js";
 export { AdminWizardSessionSql } from "./adminWizardSession.sql.js";
 export { CollectionCsvImportSql } from "./collectionCsvImport.sql.js";
 export { CompletionatorImportSql } from "./completionatorImport.sql.js";
-export { GameDbCsvImportSql } from "./gameDbCsvImport.sql.js";
-export { GameDbCsvImportMappingSql } from "./gameDbCsvImportMapping.sql.js";
 export { HltbCacheSql } from "./hltbCache.sql.js";
 export { MemberSql } from "./member.sql.js";
 export { SteamCollectionImportSql } from "./steamCollectionImport.sql.js";
